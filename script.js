858fa47acb61fd6cea4145914e48075ec2fb42ff
talor-hammond <talorjhammond@gmail.com>
2018-05-22 22:49:48 +1200

    complete second calculation function

--- script.js
+++ script.js
@@ -11,14 +11,14 @@ function calculateCorrection() {
     var goalGlucoseNumber = Number(goalGlucose);
     var correctionFactorNumber = Number(correctionFactor);
 
-    // LOG:
-    console.log(currentGlucose);
-    console.log(goalGlucose);
-    console.log(correctionFactor);
+    // // LOG:
+    // console.log(currentGlucose);
+    // console.log(goalGlucose);
+    // console.log(correctionFactor);
 
-    console.log(currentGlucoseNumber);
-    console.log(goalGlucoseNumber);
-    console.log(correctionFactorNumber);
+    // console.log(currentGlucoseNumber);
+    // console.log(goalGlucoseNumber);
+    // console.log(correctionFactorNumber);
 
     // Results panel:
     var results = document.getElementById('firstResults');
@@ -44,13 +44,6 @@ function calculateCorrection() {
         // calculating units to administer & rounding to lowest whole-number...
         var units = Math.floor((currentGlucoseNumber - goalGlucoseNumber) / correctionFactorNumber);
 
-        // // rounding units to 1dp w .toFixed...
-        // var roundedUnits = units.toFixed(1);
-
-        // // LOG:
-        // console.log(units);
-        // console.log(roundedUnits);
-
         if (units == 1) { // for an individual unit:
             results.innerHTML = '<b>Take ' + units + ' unit :~)</b><br><br>All results are rounded <i>down</i> to the nearest whole number.';            
         } else {
@@ -61,18 +54,45 @@ function calculateCorrection() {
 
 // FUNCTION FOR CALCULATION MADE WHEN EATING:
 function calculateInsulin() {
+    
+    // String reference:
+    var carbs = document.getElementById('carbohydrates').value;
+    var carbRatio = document.getElementById('carbRatio').value;
 
+    // Number reference:
+    var carbsNumber = Number(carbs);
+    var carbRatioNumber = Number(carbRatio);
 
+    // Results panel:
+    var results = document.getElementById('secondResults');
+
+    // 1. IF ANY OF THE VALUES ARE AN EMPTY STRING...
+    if ((carbs || carbRatio) == '') {
+
+        results.innerHTML = '<i>Please enter two valid numbers :~)</i>'
+    
+    // 2. IF ANY OF THE VALUES AREN'T AN EMPTY STRING, AND ANY OF WHICH ARE INVALID NUMBERS...
+    } else if ((carbs.length || carbRatio.length) > 0 && isNaN(carbRatioNumber) || isNaN(carbsNumber) == true)  {
+
+        results.innerHTML = '<i>Please enter two valid numbers :~)</i>'
+
+    // 3. IF THE RATIO IS 0; WHERE IT WOULD GIVE INFINITY UNITS
+    } else if (carbRatioNumber == 0) {
+
+        results.innerHTML = '<i>Please enter a valid ratio :~)</i>'
+
+    // 4. ELSE, CALCULATE THE NUMBERS:
+    } else {
+
+        // calculating units to administer & rounding to lowest whole-number...
+        var units = Math.floor(carbsNumber / carbRatioNumber);
+
+        if (units == 1) { // for an individual unit:
+            results.innerHTML = '<b>Take ' + units + ' unit :~)</b><br><br>All results are rounded <i>down</i> to the nearest whole number.';            
+        } else {
+            results.innerHTML = '<b>Take ' + units + ' units :~)</b><br><br>All results are rounded <i>down</i> to the nearest whole number.';
+        }
+    }
     
 }
-
-// FUNCTION FOR SHOWING RESULTS:
-// function showFirstResults() {
-
-//     var firstResults = document.getElementById('firstResults');
-
-//     // firstResults -- add css visibility: visible, here;
-//     firstResults.style.visibility = 'visible';
-
-// }
 
